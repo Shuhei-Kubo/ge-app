036af883837c7985a62fef6ccc8be0806695075f
Shuhei-Kubo <shuhei.k6221@gmail.com>
2020-08-14 17:37:15 +0900

    投稿機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
+  
+  resources :posts, only: [:new, :index, :create]
+  resources :users, only: [:edit, :index]
 
  
 
